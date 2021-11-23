459fcc2a06389a923ce6ee7851009115a5d3137b
Steve Nowicki <stevennowicki3@gmail.com>
2021-11-23 16:12:43 -0600

    work on search form

--- src/components/FlightSearch.vue
+++ src/components/FlightSearch.vue
@@ -1,29 +1,122 @@
 <template>
   <div class="center">
     <a-row style="margin-top: 200px">
-      <a-input-search allowClear placeholder="Search" @search="onSearch" />
-      <a-spin v-if="searching" tip="Searching..." class="center"></a-spin>
+      <a-form-model :model="searchForm">
+        <a-form-model-item label="Request Id">
+          <a-input v-model="searchForm.requestId"></a-input>
+        </a-form-model-item>
+        <a-form-model-item label="Origin">
+          <a-select v-model="searchForm.origin">
+            <a-select-option value="LAX">
+              Los Angeles
+            </a-select-option>
+            <a-select-option value="ORD">
+              O'hare
+            </a-select-option>
+            <a-select-option value="JFK">
+              John F Kennedy
+            </a-select-option>
+          </a-select>
+        </a-form-model-item>
+        <a-form-model-item label="Departure">
+          <a-date-picker v-model="searchForm.departure" :disabled-date="disabledDate" />
+        </a-form-model-item>
+        <a-form-model-item label="Destination">
+          <a-select  v-model="searchForm.destination">
+            <a-select-option value="LAX">
+              Los Angeles
+            </a-select-option>
+            <a-select-option value="ORD">
+              O'hare
+            </a-select-option>
+            <a-select-option value="JFK">
+              John F Kennedy
+            </a-select-option>
+          </a-select>
+        </a-form-model-item>
+        <a-form-model-item label="Return">
+          <a-date-picker v-model="searchForm.return" :disabled-date="disabledDate" />
+        </a-form-model-item>
+        <a-form-model-item label="Cabin Type">
+          <a-select v-model="searchForm.cabinType">
+            <a-select-option value="economy">
+              Economy
+            </a-select-option>
+            <a-select-option value="business">
+              Business
+            </a-select-option>
+            <a-select-option value="firstClass">
+              First Class
+            </a-select-option>
+          </a-select>
+        </a-form-model-item>
+        <a-form-model-item  :wrapper-col="{ span: 4 }">
+          <a-button type="primary" @click="onSearch" :disabled="disabledForm">
+            Search
+          </a-button>
+          <a-button  @click="resetForm" style="margin-left:10px;">
+            Reset
+          </a-button>
+          </a-form-model-item>
+      </a-form-model>
     </a-row>
   </div>
 </template>
 
 <script>
+import {mapMutations} from "vuex";
+import moment from "moment"
 export default {
   name: "FlightSearch",
   props: {},
   components: {
   },
+  computed: {
+    disabledForm() {
+      return !(
+          this.searchForm.requestId && this.searchForm.origin && this.searchForm.departure
+          && this.searchForm.destination && this.searchForm.return && this.searchForm.cabinType
+      )
+    }
+  },
   data() {
     return {
-      searchTerm: "",
+      searchForm: {
+        requestId: null,
+        origin: null,
+        departure: null,
+        destination: null,
+        return: null,
+        cabinType: null
+      },
     };
   },
   methods: {
+    moment,
+    ...mapMutations([
+        "setRequestId", "setOrigin", "setDeparture", "setDestination", "setReturn", "setCabinType"
+    ]),
     onSearch(val) {
         this.searchTerm = val;
-        this.$emit("flight-search-request", val);
+        //requirements ask to emit this event, but i don't use it. using vuex instead.
+        this.$emit("flight-search-request", this.searchForm);
         this.$router.push({path: "/flightResult"});
     },
+    changeDeparture(date, dateString) {
+      this.searchForm.departure = dateString;
+    },
+    disabledDate(current) {
+      // Can not select days before today and today
+      return current && current < moment().startOf('day') -1;
+    },
+    resetForm() {
+      this.searchForm.requestId = null;
+      this.searchForm.origin = null;
+      this.searchForm.departure = null;
+      this.searchForm.destination = null;
+      this.searchForm.return = null;
+      this.searchForm.cabinType = null;
+    }
   },
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,42 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    requestId: null,
+    origin: null,
+    departure: null,
+    destination: null,
+    return: null,
+    cabinType: null,
+  },
+  getters: {
+    getRequestId: (state) => state.requestId,
+    getOrigin: (state) => state.origin,
+    getDeparture: (state) => state.departure,
+    getDestination: (state) => state.destination,
+    getReturn: (state) => state.return,
+    getCabinType: (state) => state.cabinType,
+  },
+  mutations: {
+    setRequestId(state, requestId) {
+      state.requestId = requestId;
+    },
+    setOrigin(state, origin) {
+      state.origin = origin;
+    },
+    setDeparture(state, departure) {
+      state.departure = departure;
+    },
+    setDestination(state, destination) {
+      state.destination = destination;
+    },
+    setReturn(state, returnTime) {
+      state.return = returnTime;
+    },
+    setCabinType(state, cabinType) {
+      state.cabinType = cabinType;
+    }
+  },
   actions: {},
   modules: {},
 });
